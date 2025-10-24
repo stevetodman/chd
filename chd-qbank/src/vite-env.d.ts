9ce2481f5a607e44841c8a732f77574ac5632c5a
stevetodman <stevetodman@Hotmail.com>
2025-10-24 17:05:26 -0500

    chore: enforce full type checking

--- chd-qbank/src/vite-env.d.ts
+++ chd-qbank/src/vite-env.d.ts
@@ -1,3 +1,16 @@
-/// <reference types="vite/client" />
+interface ImportMetaEnv {
+  readonly BASE_URL: string;
+  readonly MODE: string;
+  readonly DEV: boolean;
+  readonly PROD: boolean;
+  readonly SSR: boolean;
+  readonly VITE_APP_NAME?: string;
+  readonly VITE_SUPABASE_URL?: string;
+  readonly VITE_SUPABASE_ANON_KEY?: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
 
 declare const __BUILD_HASH__: string;
