4bb27f645f296b2253cfdbd6cf6e88b02ecfb29d
stevetodman <stevetodman@Hotmail.com>
2025-10-29 13:02:11 -0500

    Guard client bootstrap from SSR

--- chd-qbank/src/main.tsx
+++ chd-qbank/src/main.tsx
@@ -9,7 +9,7 @@ import { useLocaleStore } from "./store/i18n";
 import "./styles/globals.css";
 
 async function registerServiceWorker() {
-  if (!("serviceWorker" in navigator) || !config.isProduction) {
+  if (typeof navigator === "undefined" || !("serviceWorker" in navigator) || !config.isProduction) {
     return;
   }
 
@@ -21,37 +21,39 @@ async function registerServiceWorker() {
   }
 }
 
-const root = document.getElementById("root");
+if (typeof document !== "undefined") {
+  const root = document.getElementById("root");
 
-if (!root) {
-  throw new Error("Root element not found");
+  if (!root) {
+    throw new Error("Root element not found");
+  }
+
+  const availableLocales = Object.keys(messages);
+  const localeState = useLocaleStore.getState();
+  const browserLocale = typeof navigator !== "undefined" && navigator.language ? navigator.language : FALLBACK_LOCALE;
+  const initialLocale = normalizeLocale(localeState.locale || browserLocale, availableLocales, FALLBACK_LOCALE);
+
+  if (localeState.locale !== initialLocale) {
+    useLocaleStore.setState({ locale: initialLocale });
+  }
+
+  ReactDOM.createRoot(root).render(
+    <React.StrictMode>
+      <BrowserRouter>
+        <I18nProvider
+          initialLocale={initialLocale}
+          fallbackLocale={FALLBACK_LOCALE}
+          messages={messages}
+          onLocaleChange={(nextLocale) => {
+            const { setLocale } = useLocaleStore.getState();
+            setLocale(nextLocale);
+          }}
+        >
+          <App />
+        </I18nProvider>
+      </BrowserRouter>
+    </React.StrictMode>
+  );
+
+  void registerServiceWorker();
 }
-
-const availableLocales = Object.keys(messages);
-const localeState = useLocaleStore.getState();
-const browserLocale = typeof navigator !== "undefined" && navigator.language ? navigator.language : FALLBACK_LOCALE;
-const initialLocale = normalizeLocale(localeState.locale || browserLocale, availableLocales, FALLBACK_LOCALE);
-
-if (localeState.locale !== initialLocale) {
-  useLocaleStore.setState({ locale: initialLocale });
-}
-
-ReactDOM.createRoot(root).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <I18nProvider
-        initialLocale={initialLocale}
-        fallbackLocale={FALLBACK_LOCALE}
-        messages={messages}
-        onLocaleChange={(nextLocale) => {
-          const { setLocale } = useLocaleStore.getState();
-          setLocale(nextLocale);
-        }}
-      >
-        <App />
-      </I18nProvider>
-    </BrowserRouter>
-  </React.StrictMode>
-);
-
-void registerServiceWorker();
